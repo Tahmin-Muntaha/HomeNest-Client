2f0c1d893778284250411f86774743fa77e3e0db
Tahmin-Muntaha <tahminmuntaha66@gmail.com>
2025-12-05 14:40:58 +0600

    footer

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import NavBar from '../Components/NavBar';
 import { Outlet } from 'react-router';
+import Footer from '../Components/Footer';
 
 const Root = () => {
     return (
         <div className='max-w-[1260px] mx-auto my-4 p-4'>
             <NavBar></NavBar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
